0fd3bd6110064ddfaba9123a68435be40a95e36c
DevilStyle <devilstyle87@gmail.com>
2026-01-20 09:25:50 +0100

    Add WinUI 3 views for Dashboard, Credentials, Settings, Import, and Generator with Fluent Design

---
+++ SecureVault.App/Converters/BoolNegationConverter.cs
@@ -0,0 +1,16 @@
+using Microsoft.UI.Xaml.Data;
+
+namespace SecureVault.App.Converters;
+
+public class BoolNegationConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        return value is bool boolValue ? !boolValue : value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        return value is bool boolValue ? !boolValue : value;
+    }
+}
---
+++ SecureVault.App/Views/CredentialsPage.xaml.cs
@@ -0,0 +1,54 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Navigation;
+using SecureVault.App.ViewModels;
+
+namespace SecureVault.App.Views;
+
+public sealed partial class CredentialsPage : Page
+{
+    public CredentialsViewModel ViewModel { get; }
+
+    public CredentialsPage()
+    {
+        this.InitializeComponent();
+        ViewModel = App.Services.GetRequiredService<CredentialsViewModel>();
+    }
+
+    protected override async void OnNavigatedTo(NavigationEventArgs e)
+    {
+        base.OnNavigatedTo(e);
+        await ViewModel.LoadCredentialsCommand.ExecuteAsync(null);
+    }
+
+    private async void OnSearchKeyPressed(KeyboardAccelerator sender, KeyboardAcceleratorInvokedEventArgs args)
+    {
+        await ViewModel.SearchCommand.ExecuteAsync(null);
+    }
+
+    private void OnFilterAll(object sender, RoutedEventArgs e)
+    {
+        ViewModel.FilterMode = "All";
+        ViewModel.ApplyFilterCommand.Execute(null);
+    }
+
+    private void OnFilterWeak(object sender, RoutedEventArgs e)
+    {
+        ViewModel.FilterMode = "Weak";
+        ViewModel.ApplyFilterCommand.Execute(null);
+    }
+
+    private void OnFilterDuplicate(object sender, RoutedEventArgs e)
+    {
+        ViewModel.FilterMode = "Duplicate";
+        ViewModel.ApplyFilterCommand.Execute(null);
+    }
+
+    private void OnFilterCompromised(object sender, RoutedEventArgs e)
+    {
+        ViewModel.FilterMode = "Compromised";
+        ViewModel.ApplyFilterCommand.Execute(null);
+    }
+}
